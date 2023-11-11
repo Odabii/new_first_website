6982027000622c0ac1e1fe0af430b4d31025fc36
Odabii <149680939+Odabii@users.noreply.github.com>
2023-11-11 10:28:27 +0000

    ADDED TEMPLATE

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, template_folder= "template")
+
 
 @app.route ("/")
 def Hello_world():
   return render_template ("home.html")
+
+@app.route ("/staffs")
+def staffs_officiele():
+  return jsonify (render_template ("pos.html"))
 
 if __name__ == "__main__":
   app.run (host= "0.0.0.0", debug = True)
